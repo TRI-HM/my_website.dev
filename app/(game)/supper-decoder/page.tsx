25460d4931952a9d921a99c227ee172515f0b7d3
TRÍ HUỲNH MINH 🌏 <huynhminhtri.official@gmail.com>
2025-07-27 11:04:14 +0700

    feat: implement Supper Decoder Game page with random color generation and layout

---
+++ app/(game)/supper-decoder/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const page = () => {
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const randomColors = Array.from({ length: 5 }, getRandomColor);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-300">
+      <h1 className="text-3xl font-bold text-center my-8">
+        Supper Decoder Game
+      </h1>
+      <div className="container bg-black rounded-lg p-6 shadow-lg">
+        <div className="w-full grid grid-cols-5 gap-4">
+          {randomColors.map((color, idx) => (
+            <div
+              key={idx}
+              className="bg-gray-800 text-white p-4 rounded-lg mb-4 animate-fade-in"
+              style={{ backgroundColor: color }}>
+              {idx + 1}
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-cols-5 gap-4 p-4">
+          {Array.from({ length: 10 }).map((_, rowIdx) =>
+            Array.from({ length: 5 }).map((_, colIdx) => (
+              <React.Fragment key={`${rowIdx}-${colIdx}`}>
+                <div className="border border-white rounded-md p-2 text-center">
+                  {colIdx == 4 ? "" : rowIdx * 5 + colIdx + 1}
+                </div>
+              </React.Fragment>
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
