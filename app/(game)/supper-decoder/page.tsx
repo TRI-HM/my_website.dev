5a15ec0c84400c3d56f7607a96205a72a4808401
TRÍ HUỲNH MINH 🌏 <huynhminhtri.official@gmail.com>
2025-07-27 11:13:39 +0700

    feat: add random color generation for Supper Decoder Game layout

--- app/(game)/supper-decoder/page.tsx
+++ app/(game)/supper-decoder/page.tsx
@@ -2,26 +2,33 @@ import React from "react";
 
 const page = () => {
   const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    const basicColors = [
+      "#FF0000", // Đỏ
+      "#FFA500", // Cam
+      "#FFFF00", // Vàng
+      "#008000", // Lục
+      "#0000FF", // Lam
+      "#4B0082", // Chàm
+      "#EE82EE", // Tím
+      "#FFFFFF", // Trắng
+      "#000000", // Đen
+    ];
+    const randomIndex = Math.floor(Math.random() * basicColors.length);
+    return basicColors[randomIndex];
   };
 
-  const randomColors = Array.from({ length: 5 }, getRandomColor);
+  const randomColors = Array.from({ length: 7 }, getRandomColor);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-300">
       <h1 className="text-3xl font-bold text-center my-8">
         Supper Decoder Game
       </h1>
-      <div className="container bg-black rounded-lg p-6 shadow-lg">
-        <div className="w-full grid grid-cols-5 gap-4">
+      <div className="container bg-black/40 rounded-lg p-6 shadow-lg">
+        <div className="w-full grid grid-cols-7 gap-4">
           {randomColors.map((color, idx) => (
             <div
               key={idx}
-              className="bg-gray-800 text-white p-4 rounded-lg mb-4 animate-fade-in"
+              className="bg-gray-800 w-10 h-10 text-white rounded-lg animate-fade-in"
               style={{ backgroundColor: color }}>
               {idx + 1}
             </div>
