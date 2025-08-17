1c9dfd5095d579fce4f879b2e18a28b6bc83cb0d
TRÍ HUỲNH MINH 🌏 <huynhminhtri.official@gmail.com>
2025-08-17 22:24:15 +0700

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="vi">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-// import Timeline from "./components/timeline/Timeline";
-
-import PortfolioGame from "./(pages)/portfolioGame/page";
-// import Profile from "./(pages)/profile/page";
+import Profile from "./(pages)/profile/page";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-300">
-      {/* <Profile /> */}
-      <PortfolioGame />
+      <Profile />
+      {/* <PortfolioGame /> */}
     </div>
   );
 }
